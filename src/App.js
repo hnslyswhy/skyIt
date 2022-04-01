af363836fd5c2d605b7ad41d9d5a6ce6415cd571
hnslyswhy <huanyuw01@gmail.com>
2022-04-01 16:26:48 -0700

    add certification drop drown search

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { FilterMatchMode } from 'primereact/api';
+import { Dropdown } from 'primereact/dropdown';
+
 
 
 
 function App() {
   const [data, setData] = useState(null);
+  const [first, setFirst] = useState(0);
+  const [selectedRow, setSelectedRow] = useState(null);
   const [filters, setFilters] = useState({
-    'title': { value: null, matchMode: FilterMatchMode.CONTAINS },
+    'title': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
     'releaseDate': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
     'length': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-    'director': { value: null, matchMode: FilterMatchMode.IN },
+    'director': { value: null, matchMode: FilterMatchMode.EQUALS },
     'certification': { value: null, matchMode: FilterMatchMode.EQUALS },
-    'rating': { value: null, matchMode: FilterMatchMode.EQUALS }
+    'rating': { value: null, matchMode: FilterMatchMode.STARTS_WITH }
   });
 
   useEffect(() => {
@@ -25,50 +29,58 @@ function App() {
           movie.rating = (movie.rating / 5 * 100).toFixed(2).toString() + "%";
           return movie;
         })
+        console.log(updatedData);
         setData(updatedData)
       })
       .catch(e => console.log(e))
   }, [])
 
+  //certification dropdown
+  const certifications = [{ label: 'General', value: "General" }, { label: 'CA-PG', value: "CA-PG" }, { label: '14 Accompaniment', value: "14 Accompaniment" }];
+
+  const certificateFilter = (options) => {
+    return <Dropdown style={{ width: '100%' }} className="ui-column-filter" value={options.value} options={certifications}
+      onChange={
+        (e) => {
+          options.filterApplyCallback(e.value)
+        }
+      } placeholder="Select a certification" showClear />;
+  }
+
+
 
   return (
-    <div className="App">
+    <div className="move">
+      <h1 className="title">Favorite Movie List</h1>
       <section className="table">
         <DataTable value={data}
           paginator
+          rows={10}
+          //paginator
+          first={first}
+          onPage={(e) => setFirst(e.first)}
+          //dropdown box filter
           filters={filters}
           filterDisplay="row"
+          // row selection
+          selectionMode="single"
+          selection={selectedRow} onSelectionChange={e => setSelectedRow(e.value)}
+
           responsiveLayout="scroll"
           paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-          rows={10}
+
           emptyMessage="No data found."
           currentPageReportTemplate=""
         >
-          <Column field="title" header="Title"></Column>
-          <Column field="releaseDate" header="Year"></Column>
-          <Column field="length" header="Running time"></Column>
+          <Column field="title" header="Title" filter filterPlaceholder="Search by title" showFilterMenu={false} ></Column>
+          <Column field="releaseDate" header="Year" filter filterPlaceholder="Search by year" showFilterMenu={false} ></Column>
+          <Column field="length" header="Running time" filter filterPlaceholder="Search by time" showFilterMenu={false} ></Column>
           <Column field="director" header="Director"></Column>
-          <Column field="certification" header="Certification"></Column>
-          <Column field="rating" header="Rating"></Column>
+          <Column field="certification" header="Certification" filter filterElement={certificateFilter} showFilterMenu={false} ></Column>
+          <Column field="rating" header="Rating" filter filterPlaceholder="Search by rating" showFilterMenu={false} ></Column>
         </DataTable>
       </section>
 
-      {/* <section className="card">
-        <div className="card">
-          <h5>Filter Row</h5>
-          <p>Filters are displayed inline within a separate row.</p>
-          <DataTable value={customers2} paginator className="p-datatable-customers" rows={10}
-            dataKey="id" filters={filters2} filterDisplay="row" loading={loading2} responsiveLayout="scroll"
-            globalFilterFields={['name', 'country.name', 'representative.name', 'status']} header={header2} emptyMessage="No customers found.">
-            <Column field="name" header="Name" filter filterPlaceholder="Search by name" style={{ minWidth: '12rem' }} />
-            <Column header="Country" filterField="country.name" style={{ minWidth: '12rem' }} body={countryBodyTemplate} filter filterPlaceholder="Search by country" />
-            <Column header="Agent" filterField="representative" showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '14rem' }} body={representativeBodyTemplate}
-              filter filterElement={representativeRowFilterTemplate} />
-            <Column field="status" header="Status" showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '12rem' }} body={statusBodyTemplate} filter filterElement={statusRowFilterTemplate} />
-            <Column field="verified" header="Verified" dataType="boolean" style={{ minWidth: '6rem' }} body={verifiedBodyTemplate} filter filterElement={verifiedRowFilterTemplate} />
-          </DataTable>
-        </div>
-      </section> */}
     </div>
   );
 }

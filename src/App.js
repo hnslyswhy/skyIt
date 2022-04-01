6633ee051f5c8396984c90925f53f49a60d2b487
hnslyswhy <huanyuw01@gmail.com>
2022-04-01 13:53:17 -0700

    step 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://skyit-coding-challenge.herokuapp.com/movies")
+      .then((res) => setData(res.data))
+      .catch(e => console.log(e))
+  }, [])
+
   return (
     <div className="App">
-      hi
+      {data && data.map(movie => {
+        return (<section key={movie._id}>
+          <p>{movie.title}</p>
+          <p>{movie.releaseDate}</p>
+          <p>{movie.director}</p>
+          <p>{movie.length}</p>
+          <p>{movie.rating}</p>
+        </section>)
+      })}
+
+
     </div>
   );
 }

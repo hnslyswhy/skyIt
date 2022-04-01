3334c36559218ef47c6efcf4eadb89b30d47073f
hnslyswhy <huanyuw01@gmail.com>
2022-04-01 14:13:21 -0700

    complete step 2, putting data into table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
 
 
 function App() {
@@ -11,19 +14,25 @@ function App() {
       .catch(e => console.log(e))
   }, [])
 
+  console.log(data[0])
+
   return (
     <div className="App">
-      {data && data.map(movie => {
-        return (<section key={movie._id}>
-          <p>{movie.title}</p>
-          <p>{movie.releaseDate}</p>
-          <p>{movie.director}</p>
-          <p>{movie.length}</p>
-          <p>{movie.rating}</p>
-        </section>)
-      })}
-
-
+      <section className="table">
+        <DataTable value={data} paginator responsiveLayout="scroll"
+          paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+          rows={10}
+          emptyMessage="No data found."
+          currentPageReportTemplate=""
+        >
+          <Column field="title" header="Title"></Column>
+          <Column field="releaseDate" header="Year"></Column>
+          <Column field="length" header="Running time"></Column>
+          <Column field="director" header="Director"></Column>
+          <Column field="certification" header="Certification"></Column>
+          <Column field="rating" header="Rating"></Column>
+        </DataTable>
+      </section>
     </div>
   );
 }
